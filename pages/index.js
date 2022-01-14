e5134929516d3074acf7d48c6c1696c68138fb40
johnmuthua <muthuahjohn@gmail.com>
2022-01-14 20:39:36 +0300

    created models

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1 className='bg-blue-700 py-2 px-4 rounded-full color-white align-text-center' >Test Element</h1>
+  )
+}
